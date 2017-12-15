635e3aa2435248fe3096c633192a9b8049492cbe
Cooking Pixels <cookingpixels@gmail.com>
2017-12-15 00:14:48 -0300

    Removed unused components

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import AddBookForm from '@/components/AddBookForm'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'AddBookForm',
-      component: AddBookForm
-    }
+
   ]
 })
